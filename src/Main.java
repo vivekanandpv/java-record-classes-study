a67cdc4b3d7cd81a172119f09d89f14992ef10b0
Vivekanand P V <vivek@anviksha.io>
2024-08-03 14:09:28 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,35 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point3D p1 = new Point3D(1, 2, 3);    //  parameters are mandatory
+        Point3D p2 = new Point3D(1, 2, 3);
+
+        System.out.println(p1 == p2);   //  false; different objects
+        System.out.println(p1.equals(p2));  //  equals method is implemented by default
+
+        //  Values are not mutable
+        //  p1.x = 9;   //  error
+        //  p.x(3);     //  error
+
+        //  getting the value
+        System.out.println(p1.x()); //  not as p1.x
+    }
+
+    //  record can be used as a regular type in APIs
+    static Point3D getPoint() {
+        return new Point3D(4, 5, 6);
+    }
+
+    static void consumePoint(Point3D p) {
+
     }
 }
 
-record Point3D(int x, int y, int z) {}
-
-class Point3DAsClass {
-    private final int x;
-    private final int y;
-    private final int z;
-
-    public Point3DAsClass(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public int x() {
-        return x;
-    }
-
-    public int y() {
-        return y;
-    }
-
-    public int z() {
-        return z;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point3DAsClass that = (Point3DAsClass) o;
-        return x == that.x && y == that.y && z == that.z;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y, z);
-    }
+//  x, y, and z are called components of the record
+//  all records inherit from java.lang.Record superclass
+//  https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Record.html
+//  records were introduced in Java 16
+record Point3D(int x, int y, int z) {
+    //  A canonical constructor, private final fields for components, getters,
+    //  equals and hasCode methods are implemented behind the scenes
 }
