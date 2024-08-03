24af05b919f76bde1169364ed5891f76b27a129c
Vivekanand P V <vivek@anviksha.io>
2024-08-03 20:39:15 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,24 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Point3D.PI);
-        Point3D.foo();
-    }
-
-    //  record can be used as a regular type in APIs
-    static Point3D getPoint() {
-        return new Point3D(4, 5, 6);
-    }
-
-    static void consumePoint(Point3D p) {
 
     }
 }
 
 record Point3D(int x, int y, int z) {
-    //  static members (fields and methods) can be declared in a record
-    public static final double PI;
-
-    //  static initializer can also be declared like a normal class
-    static {
-        PI = Math.PI;
+    //  instance methods can be declared (both private and public)
+    private void printMessageHelper() {
+        System.out.println("Point3D#printMessageHelper");
     }
 
-    public static void foo() {
-        System.out.println("Point3D#foo");
+    public void printMessage() {
+        printMessageHelper();
+    }
+
+    //  accessors can be explicitly defined
+    //  accessors should not be restrictive than the record type
+    //  changing the return type of the component is not allowed
+    public int x() {
+        return Math.abs(x);
     }
 }
