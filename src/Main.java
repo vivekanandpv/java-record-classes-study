2854c6640832a87884ddf23bce7b492eb6108b64
Vivekanand P V <vivek@anviksha.io>
2024-08-03 13:57:57 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+}
+
+record Point3D(int x, int y, int z) {}
+
+class Point3DAsClass {
+    private final int x;
+    private final int y;
+    private final int z;
+
+    public Point3DAsClass(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public int z() {
+        return z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point3DAsClass that = (Point3DAsClass) o;
+        return x == that.x && y == that.y && z == that.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
     }
 }
